cdf73b6b1ad024c7dd5b7bf947bb5a1d29fb1c99
Ismaaeel04 <190366965+Ismaaeel04@users.noreply.github.com>
2025-09-23 09:53:11 +0100

    Add files via upload

---
+++ LAB 1/ThermTest.java
@@ -0,0 +1,29 @@
+// Student Name 	: Oisin Cawley
+// Student Id Number: 
+// Date 			: Nov-2015
+// Purpose 			: My first class implementation
+
+public class ThermTest
+{ // begin class ThermTest
+	public static void main(String args[]) 
+	{ // being main method
+
+		double tempB;
+
+		Thermometer thermA = new Thermometer();		// Create an instance of our Thermometer class
+		Thermometer thermB = new Thermometer();
+
+		System.out.println("Temp. of Thermometer A is " + thermA.getCelsius() );
+		thermA.setCelsius(20.0);
+		System.out.println("Temp. of Thermometer A is " + thermA.getCelsius() );
+		
+		
+		thermB.setCelsius(10.0);
+
+		tempB=thermB.getCelsius();
+
+		System.out.println("Temp. of Thermometer B is " + tempB );
+		
+
+	} // end main
+} // end class ThermTest
