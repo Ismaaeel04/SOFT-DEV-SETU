bbcb85b32dd957858a4794e246b22a66e66f69c5
Ismael <190366965+Ismaaeel04@users.noreply.github.com>
2025-11-09 23:18:42 +0000

    Add files via upload

---
+++ LAB 5/Circle.java
@@ -0,0 +1,26 @@
+public class Circle extends Point {
+    
+    protected int radius;
+
+    public Circle(int x, int y, int r) {
+        
+        super(x, y);
+        setRadius(r);
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+    @Override
+
+    public String toString() {
+        return "Circle[(" + x + ", " + y + "), radius=" + radius + "]";
+    }
+
+
+}
---
+++ LAB 5/Point.java
@@ -0,0 +1,35 @@
+public class Point {
+
+    protected int x;
+    protected int y;
+    
+
+    public Point(int x, int y) {
+        setX(x);
+        setY(y);
+    }
+
+public int getX() {
+    return x;
+}   
+
+public int getY() {
+    return y;
+}
+
+public void setX(int x) {
+    this.x = x;
+}
+
+public void setY(int y) {
+    this.y = y;
+}
+
+@Override
+
+public String toString() {
+    return "Point[" + x + ", " + y + "]";
+}
+
+
+}
